22e7c4e7c448f40405d411bcc0101bed45315a09
SNAPPERITOFFICE\rahul <rahul.s@snapperit.com>
2023-05-09 08:35:04 +0530

    suxxes

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -1,28 +1,30 @@
 <?php
 
 namespace App\Controller;
+
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-
 
 class SongController extends AbstractController
 {
-    #[Route('/Api/Songs/{id<\d+>}', name: 'app_song',methods:'Get')]
-    public function getSong( int $id,LoggerInterface $logger): Response
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
-            $song=[
-                'id'=>$id,
-                'name'=>'waterfalls',
-                'url'=>'https://cdn.pixabay.com/audio/2021/09/06/audio_bacd4d6020.mp3',
-            ];
-            $logger->info('returning api respose for song {song}',[
-                'song'=>$id,
-            ]);
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
 
         return $this->json($song);
-        
     }
 }
+
